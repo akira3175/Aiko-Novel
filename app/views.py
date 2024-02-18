dc2c53c3696df78fe0aba8be3a1edea86602663d
camdao <luongcamdao@gmail.com>
2024-02-18 09:08:49 +0700

    them /search

--- app/views.py
+++ app/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'app/home.html')
+def search(request):
+    keywords = request.GET.get('keywords')
+    return render(request, 'app/search.html', {'keywords': keywords})
