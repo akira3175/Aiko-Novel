e3ef001113b9cf50d7a4401d82c548eaaeee3067
Trương Gia Hào <139223795+HaoWasabi@users.noreply.github.com>
2024-02-22 20:29:08 +0700

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your models here.
+# Change forms register django
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
 
 # Create your models here.
 class User(models.Model):
