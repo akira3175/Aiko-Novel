0d816b7cddc9e6be18b5eca18fddafe7e0eb74dd
daihuu <hodai04vn@gmail.com>
2024-04-06 22:06:11 +0700

    thêm chức năng xóa/sửa bài đăng

--- forum/urls.py
+++ forum/urls.py
@@ -1,7 +1,7 @@
 # urls.py
 
 from django.urls import path
-from .views import PostListView, PostDetailView, CreatePostView, add_comment_to_post
+from .views import PostListView, PostDetailView, CreatePostView, add_comment_to_post, EditPostView, DeletePostView
 from django.conf.urls import include
 
 
@@ -9,5 +9,7 @@ urlpatterns = [
     path('posts/', PostListView.as_view(), name='forum-post-list'),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('posts/create/', CreatePostView, name='create-post'),
+    path('post/<int:pk>/edit/', EditPostView.as_view(), name='edit-post'),
+    path('post/<int:pk>/delete/', DeletePostView.as_view(), name='delete-post'),
     path('posts/<int:post_id>/comment/', add_comment_to_post, name='add-comment-to-post'),
 ]
